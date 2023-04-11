adc850cfbb06f55a896775381a2f57633e3b0e42
MandadiSriHarsha <venkatharshachowdary@gmail.com>
2023-04-11 12:07:43 +0000

    created a clock web app that displays current time built this web app using useState useEffect and cleanup function from react hooks

---
+++ src/components/Clock/index.js
@@ -0,0 +1,30 @@
+import {useState, useEffect} from 'react'
+
+import {ClockContainer, Heading, ClockImage, Time} from './styledComponents'
+
+const Clock = () => {
+  const [date, setDate] = useState(new Date())
+
+  useEffect(() => {
+    const clearId = setInterval(() => {
+      setDate(new Date())
+    }, 1000)
+    return () => {
+      clearInterval(clearId)
+      console.log('Cleanup Function Called')
+    }
+  }, [])
+
+  return (
+    <ClockContainer>
+      <Heading>Clock</Heading>
+      <ClockImage
+        src="https://assets.ccbp.in/frontend/hooks/clock-img.png"
+        alt="clock"
+      />
+      <Time> {date.toLocaleTimeString()} </Time>
+    </ClockContainer>
+  )
+}
+
+export default Clock
